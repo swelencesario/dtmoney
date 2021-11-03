6e3c56e99e914e7b84a695a9a2abc8a2c84dd992
Swelen Cesario Ebert <swelens2@hotmail.com>
2021-11-02 23:12:55 -0300

    modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import logoImg from "../../assets/logo.svg";
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
 export function Header() {
+    const [isNewTransactionModelOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <Container>
+            <Content>
             <img src={logoImg} alt="dt money" />
-            <button type="button">
+            <button type="button" onClick={handleOpenNewTransactionModal}>
                 Nova transação
             </button>
+
+            <Modal isOpen={isNewTransactionModelOpen}
+                   onRequestClose={handleCloseNewTransactionModal}>
+                <h2>Cadastrar transação</h2>
+            </Modal>
+            </Content>
         </Container>
     )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,16 +3,15 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
-    --red: #E52E4D:
-    --blue: #5429CC;
-
+    --red: #E52E4D;
+    --blue: #5429cc;
     --blue-light: #6933ff;
-
+    --green: #33cc95;
     --text-title: #363f5f;
     --text-body: #969cb3;
-
     --shape: #ffffff;
   }
+
 * {
     margin: 0;
     padding: 0;
